b60e1ad4f65f091c9a190c41177a7ae828abd7ed
Stephatt <ttt1089@126.com>
2013-12-25 22:04:30 +0800

    mix修改

--- main.c
+++ main.c
@@ -10,53 +10,53 @@
 #include <string.h>
 #include <stdlib.h>
 
-static char sarr[9]={'1','2','3', '4', '5', '6', '7', '8', '9' };
-
+//static char sarr[9]={'1','2','3','4','5','6','7','8','9'};
 
 int main()
 {
 
     void cal(char str[]);
-    char mix(char str[]);
+    char mix(char *a,char *b);
     char itos(int i);
     void fo(char str[8],int n);
-    char str[8]={0};
+    char str[8];
+    
     fo(str,0);
     
 }
 
 
-char mix(char star[])  //数字和运算符交叉连接
+char mix(char *a,char *b)  //数字和运算符交叉连接
 {
     char mixx[17]={0};
     int i,lenth=0;
     for (i=0; i<9; i++) {
-        if (sarr[i]!='\0') {
-            mixx[lenth++]=sarr[i];
+        if (a[i]!='\0') {
+            mixx[lenth++]=a[i];
         }
-        if (star[i]!='\0') {
-            mixx[lenth++]=sarr[i];
+        if (b[i]!='\0') {
+            mixx[lenth++]=b[i];
         }
         
     }
     return mixx;
 }
 
-void cal(char str[])
+void cal(char strm[])
 {
-    int sum=0,n=0;
-    char *sb=str;
+    int sum=0,n=0,i=0;
     char strr={'+'},nchar[]={0};
-    for (; *sb!='\0'; sb++) {
-        if (*sb>='1'&&*sb<='9') {
-            n=n*10+*sb-'0';
+    for (i=0; *(strm+i)!='\0'; i++) {
+        if (*(strm+i)>='1'&&*(strm+i)<='9') {
+            n=n*10+*(strm+i)-'0';
             continue;
         }
-        sprintf(nchar, "%d",n);
-        strcat(&strr, nchar);
+        else
+            sprintf(nchar, "%d",n);  //将int型转化成字符串形式
+        strcat(strr, nchar);
         n=0;
-        if (*sb=='+'||*sb=='-') {
-            strcat(&strr, sb);
+        if (*(strm+i)=='+'||*(strm+i)=='-') {
+            strcat(strr, *(strm+i));
         }
     }
     for (char *i=&strr; *i!='\0'; i++) {
@@ -93,12 +93,23 @@ char itos(int i)
 void fo(char str[8],int n)
 {
     
-    int i;
+    int i,j,lenth=0;
+    char sarr[9]={'1','2','3','4','5','6','7','8','9'},mixx[17]={0};
     for (i=0; i<=2; i++) {
         str[n]=itos(i);
         if (n==7) {
-           cal(mix(str));
-           if(i==2)
+            for (j=0; j<9; j++) {
+                if (sarr[j]!='\0') {
+                    mixx[lenth++]=sarr[j];
+                }
+                if (str[j]!='\0') {
+                    mixx[lenth++]=str[j];
+                }
+                
+            }
+            //s=mix(*sarr,*str);
+            cal(mixx);
+            if(i==2)
               break;
         }
         else{
