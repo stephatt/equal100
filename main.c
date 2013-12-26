4df3b80b9ecffed0595cff3e0dfe1478a968d4d6
Stephatt <ttt1089@126.com>
2013-12-26 21:49:33 +0800

    枚举出所有的组合形式
    问题：如何去掉空格；如何计算其和值。

--- main.c
+++ main.c
@@ -10,112 +10,114 @@
 #include <string.h>
 #include <stdlib.h>
 
-//static char sarr[9]={'1','2','3','4','5','6','7','8','9'};
+char sarr[9]={'1','2','3','4','5','6','7','8','9'};
+#define Line 8   	//行数
+#define MAXC 2   	//每行最大字符数
 
-int main()
-{
+void MatCombin(char (*)[MAXC+1],int ,char *);
 
-    void cal(char str[]);
-    char mix(char *a,char *b);
-    char itos(int i);
-    void fo(char str[8],int n);
-    char str[8];
+char strout[Line];
+char mixx[2*Line+1];
+
+
+int main (){
+    int i;
+    char mat[Line][MAXC+1] ={"+- ","+- ","+- ","+- ","+- ","+- ","+- ","+- "};
+    MatCombin(mat, Line, strout);
+    void mix(char *a,char *b);
+    void cal(char *strm);
+    exit (0);
+}
+
+//递归地进行组合,对8个位置上的符号进行枚举
+void MatCombin(char(*p)[MAXC+1],int n,char *q){
+    int i;
+    //这个p对应最后一行，考虑输出
+    if (n==1) {
+        for (i=0; i<=MAXC; i++) {
+            if(!p[0][i]) return;
+            *q = p[0][i]; //最后一个字符写入到strout
+            puts(strout);
+            mix(sarr, strout);
+        }
+        
+    }
+    //不是最后一行，继续细分
+    else {
+        for (i=0; i<=MAXC; i++) {
+            if(!p[0][i]) return;
+            *q = p[0][i]; //写入一个字符
+            MatCombin(p+1,n-1,q+1);
+        }
+    }
     
-    fo(str,0);
     
+    return ;
 }
 
 
-char mix(char *a,char *b)  //数字和运算符交叉连接
+
+void mix(char *a,char *b)  //数字和运算符交叉连接
 {
-    char mixx[17]={0};
+
     int i,lenth=0;
     for (i=0; i<9; i++) {
         if (a[i]!='\0') {
             mixx[lenth++]=a[i];
-        }
-        if (b[i]!='\0') {
             mixx[lenth++]=b[i];
+        }
+        if (lenth<17) {
+            continue;
+        }
+        puts(mixx);
+        
+    }
+    
+    cal(mixx);
+    return ;
+}
+
+
+void cal(char *strm)
+{
+    int sum=0,n=0,i,j=0;
+    char strr[17]={0},nchar[9]={0};
+    for (i=0; strm[i]!='\0'; i++) {
+        if (strm[i]==' ') {
+            continue;
+        }
+        else if (strm[i]>='1'&&strm[i]<='9') {
+            n=n*10+strm[i]-'0';
+            continue;
+        }
+        else if (strm[i]=='+'||strm[i]=='-') {
+            sprintf(nchar,"%d",n);  //将int型转化成字符串形式
+            strr[j++]=nchar;
+            n=0;
+            strr[j++]=strm[i];
         }
         
     }
-    return mixx;
-}
-
-void cal(char strm[])
-{
-    int sum=0,n=0,i=0;
-    char strr={'+'},nchar[]={0};
-    for (i=0; *(strm+i)!='\0'; i++) {
-        if (*(strm+i)>='1'&&*(strm+i)<='9') {
-            n=n*10+*(strm+i)-'0';
-            continue;
+    for (char *q=strr; *q!='\0'; q++) {
+        if (*q=='+') {
+            sum+=*(++q);
+            break;
         }
-        else
-            sprintf(nchar, "%d",n);  //将int型转化成字符串形式
-        strcat(strr, nchar);
-        n=0;
-        if (*(strm+i)=='+'||*(strm+i)=='-') {
-            strcat(strr, *(strm+i));
+        else if (*q=='-'){
+            sum-=*(--q);
+            break;
         }
-    }
-    for (char *i=&strr; *i!='\0'; i++) {
-        switch (*i) {
-            case '+':
-                sum+=*(++i);
-                break;
-            case '-':
-                sum-=*(--i);
-                break;
-        }
+        
     }
     if (sum==100) {
-        printf("%hhd",strr);
-    }
-}
-
-char itos(int i)
-{
-    switch (i) {
-        case 0:
-            return '+';
-            break;
-        case 1:
-            return '-';
-            break;
-        case 2:
-            return ' ';
-            break;
+        printf("%s",strr);
     }
 }
 
 
-void fo(char str[8],int n)
-{
-    
-    int i,j,lenth=0;
-    char sarr[9]={'1','2','3','4','5','6','7','8','9'},mixx[17]={0};
-    for (i=0; i<=2; i++) {
-        str[n]=itos(i);
-        if (n==7) {
-            for (j=0; j<9; j++) {
-                if (sarr[j]!='\0') {
-                    mixx[lenth++]=sarr[j];
-                }
-                if (str[j]!='\0') {
-                    mixx[lenth++]=str[j];
-                }
-                
-            }
-            //s=mix(*sarr,*str);
-            cal(mixx);
-            if(i==2)
-              break;
-        }
-        else{
-           fo(str,n+1);
-        }
-    }
-}
+
+
+
+
 
 
